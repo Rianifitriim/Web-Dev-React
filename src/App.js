d01b3e6aeb3c81d8fc7915f520f1e7686e2c524d
Rianifitriim <rianifitri53@gmail.com>
2021-11-09 22:36:02 +0700

    install redux,styled components,router,tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 function App() {
   return (
     <div className="App">
      <div>
-       <h1 className="text-purple-600">Hello world</h1>
+      
      </div>
     </div>
   );
