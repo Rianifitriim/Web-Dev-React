504e87ff73ea15a47977e5b607d4b50132f2ba53
Rianifitriim <rianifitri53@gmail.com>
2021-11-09 15:08:06 +0700

    install redux,router,tailwind,styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ReactDOM from 'react-dom';
+import './index.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+       <h1 className="text-purple-600">Hello world</h1>
+     </div>
     </div>
   );
 }
