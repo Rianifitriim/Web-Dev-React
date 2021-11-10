dbedf7060f4628c95c2064bd61a212005809c0d2
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-11-10 08:40:32 +0800

    make icons and button

---
+++ src/components/styles/Button.jsx
@@ -0,0 +1,29 @@
+export const Default = {
+  default : "font-poppins font-medium py-2 px-4 filter shadow-3xl capitalize"
+}
+
+export const ButtonType = {
+  // untuk button login di navbar
+  navLogin : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full sm:w-24 rounded-full", 
+  // untuk button signup di navbar
+  navSignUpBlue : "bg-transparent text-3F70F9 border border-3F70F9 hover:bg-blue-100 w-full sm:w-24 rounded-full",
+  // untuk button sign up di navbar Home 
+  navSignUpWhite : "bg-transparent text-white border border-white hover:bg-white w-full sm:w-24 rounded-full", 
+  // untuk button di page login, signup, contact
+  loginSignUpSend : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full rounded", 
+  // untuk button buy di card Home
+  cardBuy : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full rounded-full",
+  // untuk button checkout detail product
+  detailCheckout : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full sm:w-1/2 rounded-full",
+  // untuk button checkout page cart 
+  cartCheckout : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-1/2 sm:w-32 rounded-full", 
+  // untuk button remove & remove all page cart
+  cartRemove : "bg-transparent text-3F70F9 border border-3F70F9 hover:bg-blue-100 w-1/2 sm:w-32 rounded-full", 
+}
+
+// contoh cara pakai :
+{/* 
+<Button type="cartRemove" def="default">remove</Button> 
+<Button type="cartRemove" def="default">remove all</Button> 
+*/}
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'B5C5F2' : "#B5C5F2",
+        '3F70F9' : "#3F70F9",
+      },
+      boxShadow : {
+        '3xl' : "0 15px 15px -15px rgba(0, 0, 0, 0.5)",
+      },
+      fontFamily : {
+        'poppins' : ['poppins']
+      }
+    },
   },
   variants: {
     extend: {},
