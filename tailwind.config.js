054120a4ef8d40f4e6e892b36f88596c7f5b9995
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-11-10 13:40:12 +0800

    add tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         'B5C5F2' : "#B5C5F2",
         '3F70F9' : "#3F70F9",
+        '4C79F9' : "rgba(76, 121, 249, 0.71)",
+        '69BAEC' : "#69BAEC",
       },
       boxShadow : {
         '3xl' : "0 15px 15px -15px rgba(0, 0, 0, 0.5)",
