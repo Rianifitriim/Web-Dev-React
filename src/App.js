c38aec1c5a59197d227ebaf21438671c60d41ed0
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-11-10 13:43:39 +0800

    add homepage in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import ReactDOM from 'react-dom';
-import NavbarHome from './layouts/NavbarHome';
-import NavbarNoLogin from './layouts/NavbarNoLogin';
-import NavbarLogin from './layouts/NavbarLogin';
 import Homepage from './pages/Homepage';
 
 function App() {
