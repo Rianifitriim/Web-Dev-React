b035e8d731d915a2dafcc00c00d2bf89f0710bd2
Rianifitriim <rianifitri53@gmail.com>
2021-11-11 17:59:13 +0700

    create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import react from "react";
+import StarIcon from "../icons/StarIcon"
+import Button from "../components/Button"
+export default function Card(){
+    return(
+        <div className="w-ful">
+    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12 transition-shadow ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="w-full white shadow-lg rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center transition duration-500 ease-in-out hover:shadow-gray-800 transform hover:-translate-y-1 hover:scale-110">
+                <div>
+                    <img className="object-center object-cover h-auto w-full" src="https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1168&q=80&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&h=400&q=80" alt="photo"/>
+                </div>
+                <div className="text-center py-8 sm:py-6">
+                    <p className="text-xl text-gray font-semibold mb-2 mx-3">ASUS ROG Zephyrus G14 GA401QH</p>
+                    <p className="text-left text-xl font-normal ml-5 ">Stock: 10</p>
+                    <p className="text-left text-xl font-normal ml-5 my-2">Rating : <StarIcon/>4.5</p>
+                    <p className="text-left text-xl font-normal ml-5 ">Rp.17749000</p>
+                    <div className="mx-4 my-2">
+                        <Button type="cardBuy" def="default">Buy</Button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+</div>
+    )
+}
