76d10e77297f469bf854f0f90b5299ea7fc57364
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-11-12 10:58:54 +0800

    done detail page tampilan only

--- src/components/ButtonIncDec.jsx
+++ src/components/ButtonIncDec.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function IncDec({ children }) {
   return (
     <div>
-      <button class="hover:bg-B5C5F2 font-bold py-2 px-4 rounded border- border-4">
+      <button class="hover:bg-B5C5F2 font-bold py-2 px-4 rounded border- border-4 inline-block">
         {children}
       </button>
     </div>
--- src/components/styles/Button.jsx
+++ src/components/styles/Button.jsx
@@ -14,7 +14,7 @@ export const ButtonType = {
   // untuk button buy di card Home
   cardBuy : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full rounded-full",
   // untuk button checkout detail product
-  detailCheckout : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full lg:w-1/2 rounded-full",
+  detailCheckout : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-full lg:w-1/2 rounded-full py-2 capitalize",
   // untuk button checkout page cart 
   cartCheckout : "bg-3F70F9 text-white hover:border-blue-500 hover:bg-blue-400 w-1/2 lg:w-32 rounded-full", 
   // untuk button remove & remove all page cart
