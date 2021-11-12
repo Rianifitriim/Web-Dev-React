1e184ba1c55efd30fa419d348f2f90f1a2262c7d
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-11-12 12:39:32 +0800

    done cart page tampilan only

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from 'react-dom';
-import DetailProduct from './pages/DetailProductPage';
 import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
      <div>
-      <DetailProduct />
-      {/* <Homepage /> */}
+      <Homepage />
      </div>
     </div>
   );
