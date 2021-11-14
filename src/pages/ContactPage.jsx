c7b5e657574af60f861e62305a832339ce61ee5f
Rianifitriim <rianifitri53@gmail.com>
2021-11-14 22:05:28 +0700

    finish contactpage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -8,53 +8,52 @@ import validation from "../components/validation";
 import NavbarNoLogin from "../layouts/NavbarNoLogin"
 import Footer from "../layouts/Footer";
 export default function ContactPage(){
-    const [values, setValues] = useState({
-        name: "",
-        message: "",
-      });
-    
-      const [errors, setErrors] = useState({});
-    
-      const handleChange = (event) => {
-        setValues({
-          ...values,
-          [event.target.name]: event.target.value,
-        });
-      };
-      const handleFormSubmit = (event) => {
-        event.preventDefault();
-        setErrors(validation(values));
-      };
-      
+const [values, setValues] = useState({
+name: "",
+message: "",
+});
+
+const [errors, setErrors] = useState({});
+
+const handleChange = (event) => {
+setValues({
+...values,
+[event.target.name]: event.target.value,
+});
+};
+const handleFormSubmit = (event) => {
+event.preventDefault();
+setErrors(validation(values));
+};
+
 return(
 <div>
-    <NavbarNoLogin/>
+    <NavbarNoLogin />
     <div className="container flex justify-center px-5 mt-20 ml-16">
         <div className="ml-3 mb-10 bg-B5C5F2 lg:p-8 border border-gray-300 shadow-2xl rounded-2xl lg:ml-5">
             <div className=" mx-auto sm:px-6 lg:px-8">
                 <div className="mt-2 overflow-hidden">
                     <div className="grid grid-cols-1 md:grid-cols-2">
-
                         <form className="ml-8 flex flex-col justify-center mr-10 ">
                             <div className="flex flex-col mb-4 mt-5 lg:mt-0">
                                 <label for="name" className="label text-sm font-bold text-gray-700 block">Full
                                     Name</label>
-                                <input type="name" name="name" id="name"
-                                value={values.name} onChange={handleChange} placeholder="Enter Your Name"
-                                    class="w-100 mt-2 py-2 px-3 rounded-lg bg-white dark:bg-gray-700 border border-gray-400 text-gray-800 focus:border-indigo-500 focus:outline-none"/>
-                                    <div className="text-xs text-red-600">
+                                <input type="name" name="name" id="name" value={values.name} onChange={handleChange}
+                                    placeholder="Enter Your Name"
+                                    class="w-100 mt-2 py-2 px-3 rounded-lg bg-white dark:bg-gray-700 border border-gray-400 text-gray-800 focus:border-indigo-500 focus:outline-none" />
+                                <div className="text-xs text-red-600">
                                     {errors.email && <p className="error">{errors.name}</p>}
+                                </div>
                             </div>
-                            </div>
-
                             <div class="relative mb-4">
                                 <label for="message"
                                     className="label text-sm font-bold text-gray-700 block">Message</label>
-                                <textarea id="message" name="message" value={values.message} onChange={handleChange} placeholder="Write a message..."
+                                <textarea id="message" name="message" value={values.message} onChange={handleChange}
+                                    placeholder="Write a message..."
                                     class="w-full bg-white rounded-lg border border-gray-400 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-56 text-base outline-none text-gray-800 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                                    <div className="text-xs text-red-600">
+                                <div className="text-xs text-red-600">
                                     {errors.message && <p className="error">{errors.message}</p>}
-                            </div>
+                                </div>
                             </div>
                             <div className="mt-2">
                                 <Button def="default" type="loginSignUpSend" onClick={handleFormSubmit}>Submit</Button>
@@ -73,21 +72,21 @@ return(
                                 <p>Find us</p>
                             </div>
                             <div className="flex items-center mt-4 text-black-600 dark:text-gray-400">
-                                    <IgIcon/>
+                                <IgIcon />
                                 <div className="ml-4 text-md tracking-wide font-semibold">
                                     @gos.official
                                 </div>
                             </div>
 
                             <div className="flex items-center mt-4 text-black-600 dark:text-gray-400">
-                                   <FbIcon/> 
+                                <FbIcon />
                                 <div className="ml-4 text-md tracking-wide font-semibold">
                                     g-os official
                                 </div>
                             </div>
 
                             <div className="flex items-center mt-2 text-black-600 dark:text-gray-400">
-                                <TwitterIcon/>
+                                <TwitterIcon />
                                 <div className="ml-4 text-md tracking-wide font-semibold">
                                     @gos.official
                                 </div>
@@ -97,9 +96,9 @@ return(
                 </div>
             </div>
         </div>
-    <div/>
+        <div />
+    </div>
+    <Footer />
 </div>
-<Footer/>
-</div>
-    );
-    }
+);
+}
